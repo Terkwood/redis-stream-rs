e848672c85f013406bf7150e20e1ae439852f353
terkwood <38859656+Terkwood@users.noreply.github.com>
2021-05-23 15:31:42 -0400

    make a plan

--- src/time_ordered_consumers.rs
+++ src/time_ordered_consumers.rs
@@ -1,10 +1,15 @@
-#[cfg(feature = "tocs")]
 use crate::consumer::*;
+use anyhow::{Context, Result};
 
 pub struct TimeOrderedConsumers {
   consumers: Vec<Consumer>,
 }
 
 impl TimeOrderedConsumers {
-  fn consume(&self) {}
+  fn consume(&self) -> Result<()> {
+    todo!("sort by xid");
+    todo!("copy paste a bunch from consumer.consume");
+    todo!("call something like consumer.process_message... BUT");
+    todo!("xack a bunch of things at once to reduce calls? maybe?")
+  }
 }
